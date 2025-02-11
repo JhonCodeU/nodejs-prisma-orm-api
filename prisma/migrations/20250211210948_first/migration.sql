abd3a2a200daaaca363fbfba2cd9e2b6d12e64c6
Jhon Arcila Castaño <jhon.arcila@datasoft.com.co>
2025-02-11 16:15:35 -0500

    the model is created and migration

---
+++ prisma/migrations/20250211210948_first/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "stock" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
